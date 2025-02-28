05606e9e5d4870501378722535ba523013dfb832
Finnbmb <finn.beimborn@mnd.thm.de>
2025-02-28 18:45:49 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
 private final class Main {
-  private Main() {  // Privater Konstruktor, um Instanziierung zu verhindern
-    throw new UnsupportedOperationException("Diese Klasse darf nicht instanziiert werden.");
-    }
   public static void main(final String[] args) {
     System.out.println("Hello GitHub!");
   }
