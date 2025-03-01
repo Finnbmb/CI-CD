69c8c84e9008f226df18df127d9ec5ffb50b0364
Finn <finn.beimborn@mnd.thm.de>
2025-03-01 12:54:49 +0100

    Demo.java Update and package created

---
+++ src/java/com/projekt/main/Demo.java
@@ -0,0 +1,16 @@
+package src.java.com.projekt.main;
+
+public final class Demo {
+  private Demo() {
+    throw new UnsupportedOperationException("Muss so");
+  }
+
+  /**
+   * Die Hauptmethode, die die Anwendung startet.
+   *
+   * @param args Kommandozeilenargumente
+   */
+  public static void main(final String[] args) {
+    System.out.println("Einfacher Test");
+  }
+}
